d69bb32b280786edf8b47eb78f6dc811e9254440
Aleksandr <alexkolpak2003@gmail.com>
2022-02-12 11:04:33 +0500

    Add project files.

---
+++ AuthorizedWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for AuthorizedWindow.xaml
+    /// </summary>
+    public partial class AuthorizedWindow : Window
+    {
+        private string connectionString;
+        public AuthorizedWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Test.Properties.Settings.code_developmentConnectionString"].ConnectionString;
+            this.Loaded += (sender, e) =>
+            {
+                try
+                {
+                    using (var conn = new SqlConnection(connectionString))
+                    {
+                        conn.Open();
+                        MessageBox.Show("Соединение установлено");
+
+                        var data = new DataTable();
+                        var adapter = new SqlDataAdapter("select * from dbo.Студенты", conn);
+                        adapter.Fill(data);
+                        dataGrid.ItemsSource = data.DefaultView;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Соединение не установлено");
+                }                
+            };
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string connectionString;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["Test.Properties.Settings.code_developmentConnectionString"].ConnectionString;
+            bRegister.Click += BRegister_Click;
+            bLogin.Click += BLogin_Click;
+        }
+
+        private void BLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                using(var conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    var command = new SqlCommand("select * from dbo.Аккаунты " +
+                        $"where dbo.Аккаунты.Login = {tBLogin.Text} " +
+                        $"and dbo.Аккаунты.Password = {tBPassword.Text}", conn);
+                    var reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                        while (reader.Read())
+                        {
+                            var fio = reader.GetValue(1).ToString();
+                            MessageBox.Show($"Добро пожаловать {fio}");
+                            var authorized = new AuthorizedWindow();
+                            authorized.Show();
+                        }
+                    else
+                        MessageBox.Show("Такого пользователя не существует");
+                    reader.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введите корректные данные");
+            }
+        }
+
+        private void BRegister_Click(object sender, RoutedEventArgs e)
+        {
+            var registerWindow = new RegistrationWindow();
+            registerWindow.Show();
+        }
+    }
+}
